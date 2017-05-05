39b260a0ecb182b81477b8e76906cab243b5d9ba
karimabd84 <abdelkarim.esti@gmail.com>
2017-05-05 09:40:21 +0100

    Projektdateien hinzufügen.

---
+++ CordovaPvKompass/scripts/index.ts
@@ -0,0 +1,39 @@
+﻿// Eine Einführung zur leeren Vorlage finden Sie in der folgenden Dokumentation:
+// http://go.microsoft.com/fwlink/?LinkID=397705
+// So debuggen Sie Code beim Seitenladen in Ripple oder auf Android-Geräten/-Emulatoren: Starten Sie die App, legen Sie Haltepunkte fest, 
+// und führen Sie dann "window.location.reload()" in der JavaScript-Konsole aus.
+module CordovaPvKompass {
+    "use strict";
+
+    export module Application {
+        export function initialize() {
+            document.addEventListener('deviceready', onDeviceReady, false);
+        }
+
+        function onDeviceReady() {
+            // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
+            document.addEventListener('pause', onPause, false);
+            document.addEventListener('resume', onResume, false);
+
+            // TODO: Cordova wurde geladen. Führen Sie hier eine Initialisierung aus, die Cordova erfordert.
+            var parentElement = document.getElementById('deviceready');
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        }
+
+        function onPause() {
+            // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
+        }
+
+        function onResume() {
+            // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
+        }
+
+    }
+
+    window.onload = function () {
+        Application.initialize();
+    }
+}
